7bf128eb14126110a95b14623d6ecdf50510e83c
Sean Connolly <connolly.st@gmail.com>
2013-09-08 17:40:35 -0400

    initial commit

---
+++ src/main/java/com/seaniscool/conanizer/MainActivity.java
@@ -0,0 +1,55 @@
+package com.seaniscool.conanizer;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.TextureView;
+
+/**
+ * @author Sean Connolly
+ */
+public class MainActivity extends Activity implements
+		TextureView.SurfaceTextureListener {
+
+	private Camera mCamera;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		TextureView mTextureView = new TextureView(this);
+		mTextureView.setSurfaceTextureListener(this);
+
+		setContentView(mTextureView);
+	}
+
+	public void onSurfaceTextureAvailable(SurfaceTexture surface, int width,
+			int height) {
+		mCamera = Camera.open();
+
+		try {
+			mCamera.setPreviewTexture(surface);
+			mCamera.startPreview();
+		} catch (IOException ioe) {
+			// Something bad happened
+		}
+	}
+
+	public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width,
+			int height) {
+		// Ignored, Camera does all the work for us
+	}
+
+	public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+		mCamera.stopPreview();
+		mCamera.release();
+		return true;
+	}
+
+	public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+		// Invoked every time there's a new Camera preview frame
+	}
+
+}
